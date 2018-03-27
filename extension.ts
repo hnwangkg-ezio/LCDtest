7d084336c7b07352a801072b18c713097d9cf6aa
Ezio <hnwangkg@qq.com>
2018-03-26 19:14:43 -0700

    first commit

---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace lcd {
+    /**
+     * Help goes here.
+     */
+    //% shim=lcd::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
